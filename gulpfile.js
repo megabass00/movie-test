b2f7760d207b4282e01a9ff84bab6237977af6a1
Luis Izquierdo <luisma@jig.es>
2019-03-07 13:04:35 +0100

    Gulp task added

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const jade = require('gulp-jade');
+const inject = require('gulp-inject');
+
+var config = {
+    bootstrapDir: './bower_components/bootstrap-sass',
+    devDir: './dev',
+    sassPath: './dev/css',
+    distDir: './dist'
+};
+
+gulp.task('watch', () => {
+    gulp.watch(config.sassPath + '/*.scss', gulp.series('css'));
+    gulp.watch(config.devDir + '/**/*.jade', gulp.series('templates'));
+})
+
+gulp.task('css', () => {
+    return gulp.src(config.sassPath + '/app.scss')
+                .pipe(sass({
+                    includePaths: [config.bootstrapDir + '/assets/stylesheets'],
+                }))
+                .pipe(gulp.dest(config.distDir + '/css'));
+});
+
+gulp.task('fonts', () => {
+    return gulp.src(config.bootstrapDir + '/assets/fonts/**/*')
+                .pipe(gulp.dest(config.distDir + '/fonts'));
+});
+
+gulp.task('templates', () => {
+    return gulp.src(config.devDir + '/views/**/*.jade')
+                .pipe(jade({
+                    pretty: true
+                }))
+                .pipe(gulp.dest(config.distDir));
+});
+
+
+gulp.task('default', gulp.parallel('css', 'fonts', 'templates'));
